993f979f5ef1aa3576a093a66ef3784e44eb7867
Aranjan0212 <Aranjan0212>
2024-01-02 14:36:50 +0530

    logation city wise done woring on get car

--- app/src/main/java/com/example/uberremake/Common.java
+++ app/src/main/java/com/example/uberremake/Common.java
@@ -15,6 +15,10 @@ import androidx.core.app.NotificationCompat;
 
 import com.example.uberremake.model.DriverInfoModel;
 import com.example.uberremake.service.MyFirebaseMessagingService;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Common {
     public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
@@ -34,6 +38,39 @@ public class Common {
             return "";
         }
     }
+    public static List<LatLng> decodePoly(String encoded) {
+        List poly = new ArrayList();
+        int index=0,len=encoded.length();
+        int lat=0,lng=0;
+        while(index < len)
+        {
+            int b,shift=0,result=0;
+            do{
+                b=encoded.charAt(index++)-63;
+                result |= (b & 0x1f) << shift;
+                shift+=5;
+
+            }while(b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1):(result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do{
+                b = encoded.charAt(index++)-63;
+                result |= (b & 0x1f) << shift;
+                shift +=5;
+            }while(b >= 0x20);
+            int dlng = ((result & 1)!=0 ? ~(result >> 1): (result >> 1));
+            lng +=dlng;
+
+            LatLng p = new LatLng((((double)lat / 1E5)),
+                    (((double)lng/1E5)));
+            poly.add(p);
+        }
+        return poly;
+    }
+
 
     public static void showNotification(Context context, int id, String title, String body, Intent intent) {
         PendingIntent pendingIntent=null;
